3cf3d861da08e8e785b91b823381000a4bd3ff59
Irha Ali <irhaali_97@hotmail.ca>
2022-12-19 17:36:52 -0500

    eslint

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,13 @@
 const client = require('request');
 
 // Make an http request and wait for the response.
+// createFile is a callback
 const pageFetcher = (url, createFile) => {
-  client.get(url, (error, response) => {
-    let body;
+  client(url, (error, response, body) => {
     let bodySize;
     if (response.statusCode !== 200) {
       error = `Status Code: ${response.statusCode}, (See https://http.cat/${response.statusCode} for more info)`;
     } else {
-      body = response.body;
-      // FIGURE OUT HOW TO GET THE FILESIZE
       bodySize = body.length;
     }
     createFile(error, body, bodySize);
